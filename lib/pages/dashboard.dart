1ee80755239c97c8a45c24791c3b41a14722662b
GloriaChebe <gloriachebet024@gmail.com>
2024-05-28 18:42:25 +0300

    changed dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -4,8 +4,6 @@ import 'package:shoppy/pages/profile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-
 void main() {
   runApp(const MyApp());
 }
@@ -37,21 +35,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _searchController = TextEditingController();
 
-  List<Map<String, String>> categories = [
-    {'name': 'Electronics', 'image': 'https://tinyurl.com/5d5bwe8a'},
-    {'name': 'Clothing', 'image': 'https://tinyurl.com/2y3hc96z'},
-    {'name': 'Shoes', 'image': 'https://tinyurl.com/3b5reu78'},
-    {'name': 'Books', 'image': 'https://tinyurl.com/56ketjmw'},
-    {'name': 'Home & Kitchen', 'image': 'https://tinyurl.com/4edk89yx'},
-    {'name': 'Toys', 'image': 'https://tinyurl.com/mbdc4nt9'},
+  List<Product> coffeeProducts = [
+    Product(name: 'Espresso', image: 'espresso.jpg', price: 2.99),
+    Product(name: 'Cappuccino', image: 'cappuccino.jpg', price: 3.99),
+    Product(name: 'Latte', image: 'latte.jpg', price: 4.49),
+    Product(name: 'Americano', image: 'americano.jpg', price: 2.49),
+    Product(name: 'Mocha', image: 'mocha.jpg', price: 4.99),
+    Product(name: 'Macchiato', image: 'macchiato.jpg', price: 3.79),
+    Product(name: 'Flat White', image: 'flat_white.jpg', price: 3.29),
+    Product(name: 'Irish Coffee', image: 'irish_coffee.jpg', price: 5.99),
   ];
 
-  List<Map<String, String>> displayedCategories = [];
+  List<Product> displayedProducts = [];
 
   @override
   void initState() {
     super.initState();
-    displayedCategories = List.from(categories);
+    displayedProducts = List.from(coffeeProducts);
   }
 
   @override
@@ -65,11 +65,11 @@ class _MyHomePageState extends State<MyHomePage> {
             child: TextField(
               controller: _searchController,
               decoration: const InputDecoration(
-                hintText: 'Search Categories...',
+                hintText: 'Search Products...',
                 border: InputBorder.none,
                 icon: Icon(Icons.search),
               ),
-              onChanged: _filterCategories,
+              onChanged: _filterProducts,
             ),
           ),
           IconButton(
@@ -96,107 +96,42 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          // Trending Section
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Trending',
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          Container(
-            height: 200.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 6,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ProductItem(
-                    product: Product(
-                      name: 'Trending Product ${index + 1}',
-                      image: 'trending_product_${index + 1}.jpg',
-                      price: (index + 1) * 20.0,
-                    ),
-                    onAddToCart: (product) {
-                      _addToCart(context, product);
-                    },
-                  ),
-                );
-              },
-            ),
-          ),
-         Text("Categories", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),),
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(10.0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 10.0,
-                childAspectRatio: 1.0,
-              ),
-              itemCount: displayedCategories.length,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CategoryScreen(category: displayedCategories[index]['name']!),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    elevation: 3.0,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: Image.network(
-                            displayedCategories[index]['image']!,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            displayedCategories[index]['name']!,
-                            style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          childAspectRatio: 2 / 3,
+        ),
+        itemCount: displayedProducts.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ProductItem(
+            product: displayedProducts[index],
+            onAddToCart: (product) {
+              _addToCart(context, product);
+            },
+          );
+        },
       ),
     );
   }
 
-  void _filterCategories(String query) {
+  void _filterProducts(String query) {
     if (query.isEmpty) {
       setState(() {
-        displayedCategories = List.from(categories);
+        displayedProducts = List.from(coffeeProducts);
       });
       return;
     }
-    final filtered = categories.where((category) {
-      final categoryName = category['name']!.toLowerCase();
+    final filtered = coffeeProducts.where((product) {
+      final productName = product.name.toLowerCase();
       final searchLower = query.toLowerCase();
-      return categoryName.contains(searchLower);
+      return productName.contains(searchLower);
     }).toList();
 
     setState(() {
-      displayedCategories = filtered;
+      displayedProducts = filtered;
     });
   }
 
@@ -220,100 +155,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-class CategoryScreen extends StatelessWidget {
-  final String category;
-
-  const CategoryScreen({Key? key, required this.category}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    List<Product> products = _getProductsForCategory(category);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(category),
-        centerTitle: true,
-      ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10.0),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-          childAspectRatio: 2 / 3,
-        ),
-        itemCount: products.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ProductItem(
-            product: products[index],
-            onAddToCart: (product) {
-              _addToCart(context, product);
-            },
-          );
-        },
-      ),
-    );
-  }
-
-
-  List<Product> _getProductsForCategory(String category) {
-   
-    switch (category) {
-      case 'Electronics':
-        return [
-          Product(name: 'Laptop', image: 'assets/images/laptop.jpeg', price: 999.99),
-          Product(name: 'Smartphone', image: 'assets/images/smartphone.jpeg', price: 699.99),
-        ];
-      case 'Clothing':
-        return [
-          Product(name: 'T-shirt', image: 'assets/images/tshirt.jpeg', price: 19.99),
-          Product(name: 'Jeans', image: 'assets/images/jeans.jpeg', price: 49.99),
-        ];
-      case 'Shoes':
-        return [
-          Product(name: 'Sneakers', image: 'sneakers.jpg', price: 59.99),
-          Product(name: 'Boots', image: 'boots.jpg', price: 89.99),
-        ];
-      case 'Books':
-        return [
-          Product(name: 'Novel', image: 'assets/images/novel.jpeg', price: 14.99),
-          Product(name: 'Biography', image: 'assets/images/biography.jpg', price: 24.99),
-        ];
-      case 'Home & Kitchen':
-        return [
-          Product(name: 'Blender', image: 'assets/images/blender.jpeg', price: 49.99),
-          Product(name: 'Coffee Maker', image: 'assets/images/coffee_maker.jpg', price: 39.99),
-        ];
-      case 'Toys':
-        return [
-          Product(name: 'Action Figure', image: 'assets/images/action_figure.jpg', price: 24.99),
-          Product(name: 'Board Game', image: "assets/images/board_game.jpg", price: 34.99),
-        ];
-      default:
-        return [];
-    }
-  }
-
-  void _addToCart(BuildContext context, Product product) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Item Added to Cart'),
-          content: Text('${product.name} has been added to your cart.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
-
-
