76ebe019b8cba8af01f2b40f0a8c05fa3a36f19d
Noel <noelwekulo11@gmail.com>
2024-05-29 19:20:18 +0300

    Add new pages and update existing files

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shoppy/pages/dashboard.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-
+import 'package:shoppy/pages/cart_provider.dart'; // Import the CartProvider class
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartProvider(), // Provide an instance of CartProvider
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     debugShowCheckedModeBanner: false,
-      home:  MyHomePage(),
+      title: 'Shopeasy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFFF5F5DC),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
+
+
+
+
